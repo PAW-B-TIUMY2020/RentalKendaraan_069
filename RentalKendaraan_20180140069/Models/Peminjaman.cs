821dd7802f9397fae65a075bb50b2d7eff3f2bf5
Fadia Rani <fadiarani9@gmail.com>
2020-10-31 12:38:32 +0700

    exercise

--- RentalKendaraan_20180140069/Models/Peminjaman.cs
+++ RentalKendaraan_20180140069/Models/Peminjaman.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace RentalKendaraan_20180140069.Models
 {
@@ -9,16 +10,24 @@ namespace RentalKendaraan_20180140069.Models
         {
             Pengembalian = new HashSet<Pengembalian>();
         }
-
+        [DisplayName("ID")]
         public int IdPeminjaman { get; set; }
+        [DisplayName("Tanggal Peminjaman")]
         public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
         public int? IdKendaraan { get; set; }
+        [DisplayName("Costumer")]
         public int? IdCustomer { get; set; }
+        [DisplayName("Jaminan")]
         public int? IdJaminan { get; set; }
+        [DisplayName("Biaya")]
         public int? Biaya { get; set; }
+        [DisplayName("Customer")]
 
         public Customer IdCustomerNavigation { get; set; }
+        [DisplayName("Jaminan")]
         public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
         public Kendaraan IdKendaraanNavigation { get; set; }
         public ICollection<Pengembalian> Pengembalian { get; set; }
     }
